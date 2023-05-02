87e95d24e1a98938fa1f8aee4c5f732b1357b41b
Pasindu-Lakmal <pasindu1lak@gmail.com>
2023-05-02 17:35:06 +0530

    createNodeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { CardComponent } from './card/card.component';
 import { LayoutModule } from '@progress/kendo-angular-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChartPureComponent } from './chart-pure/chart-pure.component';
+import { CentralNodeComponent } from './central-node/central-node.component';
+import { ChildNodeComponent } from './child-node/child-node.component';
 
 
   
@@ -17,7 +19,9 @@ import { ChartPureComponent } from './chart-pure/chart-pure.component';
     AppComponent,
     ChartComponent,
     CardComponent,
-    ChartPureComponent
+    ChartPureComponent,
+    CentralNodeComponent,
+    ChildNodeComponent
   ],
   imports: [
     BrowserModule,
