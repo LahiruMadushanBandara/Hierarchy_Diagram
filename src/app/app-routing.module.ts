c99dad4fa47727caa196c10922d7962fd1696783
lahiru <lahiru@theavo.net>
2023-06-26 09:59:03 +0530

    element changes 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChartComponent } from './chart/chart.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'chart', pathMatch: 'full' },
-  { path: 'chart', component: ChartComponent },
+  //{ path: '', redirectTo: 'chart', pathMatch: 'full' },
+  { path: '', component: AppComponent },
 ];
 
 @NgModule({
