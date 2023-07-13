c4dcb0981cb16903ace3e1e10960ca0b9fb0774d
Pasindu-Lakmal <pasindu1lak@gmail.com>
2023-07-13 23:47:04 +0530

    RemoveDataConnectionsArray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,6 @@ declare var $: any;
 export class AppComponent implements OnInit, AfterViewInit {
   @ViewChild('diagram', { static: false }) diagram: any;
   @ViewChild('buttonContainer', { static: true }) buttonContainer: ElementRef;
-  private dataItem;
 
   riskTemplate: string = '';
   controlTemplate: string = '';
@@ -34,11 +33,13 @@ export class AppComponent implements OnInit, AfterViewInit {
     private cdr: ChangeDetectorRef,
     private dataService: DataService
   ) {}
+  private dataConnection = this.dataService.dataConnections;
   ngAfterViewInit(): void {}
 
   ngOnInit(): void {
     sessionStorage.clear();
-
+    var dataConnection = this.dataService.dataConnections;
+    var originalData = this.dataService.originalData;
     var tempTitleDetail = '';
     var isExpanded = false;
 
@@ -78,7 +79,7 @@ export class AppComponent implements OnInit, AfterViewInit {
 
     function visualTemplate(options: any) {
       var Templates = new TemplateStructureClass();
-
+      var dataConnection = this.dataConnection;
       // return renderTemplate(Templates , options , isExpanded);
 
       var dataItem = options.dataItem;
@@ -381,8 +382,6 @@ export class AppComponent implements OnInit, AfterViewInit {
       return arrangedNodes;
     }
 
-    var originalData = this.dataService.originalData;
-
     const arrangedData = arrangeNodes(originalData, 420);
 
     $(function () {
@@ -413,43 +412,6 @@ export class AppComponent implements OnInit, AfterViewInit {
           dataShapes = JSON.parse(sessionStorage.getItem('shapes'));
         }
 
-        var dataConnections = [
-          { Id: 0, FromShapeId: 1, ToShapeId: 2, Text: null },
-          { Id: 1, FromShapeId: 2, ToShapeId: 3, Text: null },
-          { Id: 2, FromShapeId: 3, ToShapeId: 4, Text: null },
-          { Id: 3, FromShapeId: 4, ToShapeId: 5, Text: null },
-          { Id: 4, FromShapeId: 1, ToShapeId: 6, Text: null },
-          { Id: 5, FromShapeId: 6, ToShapeId: 7, Text: null },
-          { Id: 6, FromShapeId: 7, ToShapeId: 8, Text: null },
-          { Id: 7, FromShapeId: 8, ToShapeId: 9, Text: null },
-          { Id: 8, FromShapeId: 1, ToShapeId: 10, Text: null },
-          { Id: 9, FromShapeId: 10, ToShapeId: 11, Text: null },
-
-          { Id: 10, FromShapeId: 1, ToShapeId: 12, Text: null },
-          { Id: 11, FromShapeId: 12, ToShapeId: 13, Text: null },
-          { Id: 12, FromShapeId: 13, ToShapeId: 14, Text: null },
-          { Id: 13, FromShapeId: 14, ToShapeId: 15, Text: null },
-          { Id: 14, FromShapeId: 1, ToShapeId: 16, Text: null },
-          { Id: 15, FromShapeId: 16, ToShapeId: 17, Text: null },
-          { Id: 16, FromShapeId: 17, ToShapeId: 18, Text: null },
-          { Id: 17, FromShapeId: 18, ToShapeId: 19, Text: null },
-          { Id: 18, FromShapeId: 1, ToShapeId: 20, Text: null },
-          { Id: 19, FromShapeId: 20, ToShapeId: 21, Text: null },
-
-          { Id: 20, FromShapeId: 1, ToShapeId: 22, Text: null },
-          { Id: 21, FromShapeId: 1, ToShapeId: 23, Text: null },
-          { Id: 22, FromShapeId: 1, ToShapeId: 24, Text: null },
-          { Id: 23, FromShapeId: 1, ToShapeId: 25, Text: null },
-          { Id: 24, FromShapeId: 1, ToShapeId: 26, Text: null },
-          { Id: 25, FromShapeId: 1, ToShapeId: 27, Text: null },
-          { Id: 26, FromShapeId: 1, ToShapeId: 28, Text: null },
-          { Id: 27, FromShapeId: 1, ToShapeId: 29, Text: null },
-          { Id: 28, FromShapeId: 1, ToShapeId: 30, Text: null },
-          { Id: 29, FromShapeId: 1, ToShapeId: 31, Text: null },
-          { Id: 30, FromShapeId: 1, ToShapeId: 32, Text: null },
-          { Id: 23, FromShapeId: 1, ToShapeId: 33, Text: null },
-          { Id: 24, FromShapeId: 1, ToShapeId: 34, Text: null },
-        ];
         var kendoDiagram = $('#diagram').kendoDiagram({
           dataSource: {
             data: dataShapes,
@@ -483,7 +445,7 @@ export class AppComponent implements OnInit, AfterViewInit {
             },
           },
           connectionsDataSource: {
-            data: dataConnections,
+            data: dataConnection,
             schema: {
               model: {
                 id: 'id',
