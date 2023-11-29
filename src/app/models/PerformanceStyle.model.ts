ff6cf6b103d82db46a8fe4cd03a147742be2a107
Pasindu-Lakmal <pasindu1lak@gmail.com>
2023-11-29 15:34:21 +0530

    performnceClassChangeAndIssueFixes

---
+++ src/app/models/PerformanceStyle.model.ts
@@ -0,0 +1,9 @@
+export interface PerformanceStyleKPI {
+  headerStyleKPI: string;
+  boadyStyleKPI: string;
+}
+
+export interface PerformanceStyleGeneral {
+  headerStyle: string;
+  boadyStyle: string;
+}
